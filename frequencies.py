b6287f44d547c0de20abd3bb6c2d2a6c0da8c257
Maneet Sigtia <sigtiamaneet@gmail.com>
2023-09-25 01:06:30 +0100

    Submit frequencies

--- frequencies.py
+++ frequencies.py
@@ -3,12 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
-    
+
     # Your code goes here
     for a in items:
         new_a = str(a)
 
-        if new_a in items:
+        if new_a in frequencies:
             frequencies[new_a] += 1
         else:
             frequencies[new_a] = 1
